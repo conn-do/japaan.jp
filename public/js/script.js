a2cd89b377d74f701dd8e0bd844bb2208ce0183b
conn-do <kondo@japaan.jp>
2026-05-06 01:30:21 +0900

    文章とデザインを変更

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,7 @@
 $(function() {
     var DateTime = luxon.DateTime;
-    
+
     $('.from2014').html(getDiffYears('2014'));
-    $('.from2015').html(getDiffYears('2015'));
-    $('.from2017').html(getDiffYears('2017'));
-    $('.from2020').html(getDiffYears('2020'));
 
     function getDiffYears(fromYear) {
         return Math.round(
